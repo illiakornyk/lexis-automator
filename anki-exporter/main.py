b7364965297d685a1c881afb8e8a4d769eadd426
Illia Kornyk <krekerbrow@gmail.com>
2026-05-30 08:23:28 +0300

    fix(anki-exported): get port from env

--- anki-exporter/main.py
+++ anki-exporter/main.py
@@ -28,4 +28,7 @@ async def generate_deck(req: DeckRequest) -> GenerateResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
